d347d1b22f23a3577c51fe386968929f5e6289f1
“coderasha” <ashadipta@gmail.com>
2021-04-15 19:38:18 +0530

    mint and cap removed

--- contracts/UnoToken.sol
+++ contracts/UnoToken.sol
@@ -2,8 +2,7 @@ pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Capped.sol";
+
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
@@ -12,11 +11,11 @@ import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 
 
-contract UnoToken is ERC20 , ERC20Detailed , ERC20Mintable , ERC20Burnable , ERC20Capped , ERC20Pausable , ReentrancyGuard {
+contract UnoToken is ERC20 , ERC20Detailed , ERC20Burnable , ERC20Pausable , ReentrancyGuard {
   constructor (uint256 _totalSupply)
     
     ERC20Detailed("UnoRe" , "UNO",18)
-    ERC20Capped(384649206*10**18)
+  
 
         public
         {
